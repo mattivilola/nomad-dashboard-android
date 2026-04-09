b2958feaf8a51374c8f000655deb7676d483c003
Matti Vilola <matti@vilola.com>
2026-04-09 13:27:00 +0200

    Allow overriding Play Store release status during track publishing

--- scripts/publish-track.sh
+++ scripts/publish-track.sh
@@ -8,9 +8,17 @@ if [[ $# -lt 1 ]]; then
 fi
 
 track="$1"
+release_status_override="${NOMAD_PLAY_RELEASE_STATUS:-}"
 
 assert_signing_env
 assert_play_env
 
+export_android_env
+load_local_env
+
 export NOMAD_PLAY_TRACK="$track"
-run_gradle :app:publishReleaseBundle
+if [[ -n "$release_status_override" ]]; then
+  export NOMAD_PLAY_RELEASE_STATUS="$release_status_override"
+fi
+
+"$(gradlew)" :app:publishReleaseBundle
